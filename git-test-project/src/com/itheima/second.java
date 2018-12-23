9937c9375e271e233bb3440ab24b471347cad99d
itheima <itheimaqq.com>
2018-12-23 21:00:02 +0800

    div第二次提交

--- git-test-project/src/com/itheima/second.java
+++ git-test-project/src/com/itheima/second.java
@@ -3,12 +3,9 @@ package com.itheima;
 public class second {
     public static void main(String[] args) {
         System.out.println("second");
-        System.out.println("second");
-        System.out.println("second");
-        System.out.println("second");
-        System.out.println("second");
-        System.out.println("second");
-        System.out.println("second");
+       //second
+       //second
+       //second
 
 
 
